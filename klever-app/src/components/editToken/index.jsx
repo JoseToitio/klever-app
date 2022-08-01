79ba4b114675b6c43fbecfaa5b5df6eb6f323ada
JoseToitio <josetoitio@gmail.com>
2022-08-01 20:27:59 -0300

    feat: validação token existente

--- klever-app/src/components/editToken/index.jsx
+++ klever-app/src/components/editToken/index.jsx
@@ -2,21 +2,29 @@ import React, { useState } from "react";
 import Header from "../../components/header";
 import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
+import { useNavigate } from "react-router-dom";
 import "./editToken.css";
 
-export default function TokenForm({namePage}) {
-  const [inputToken, setInputToken] = useState('');
-  const [inputBalance, setInputBalance] = useState('');
-
-  let tokens = JSON.parse(localStorage.getItem('tokens')) || [];
+export default function TokenForm({ namePage }) {
+  const [inputToken, setInputToken] = useState("");
+  const [inputBalance, setInputBalance] = useState("");
+  const navigate = useNavigate();
+  let tokens = JSON.parse(localStorage.getItem("tokens")) || [];
 
   const addToLocalStorage = () => {
-    tokens.push({
-      token: inputToken,
-      balance: inputBalance
-    })
-    localStorage.setItem('tokens', JSON.stringify(tokens));
-  }
+    const getToken = JSON.parse(localStorage.getItem("tokens")) || [];
+    if (!inputToken || !inputBalance) {
+      alert("Preencha os dados!");
+    } else if (getToken.filter((t) => t.token === inputToken).length > 0) {
+      alert("Token ja existe")
+    } else {
+      tokens.push({
+        token: inputToken,
+        balance: inputBalance,
+      });
+      localStorage.setItem("tokens", JSON.stringify(tokens));
+    }
+  };
   return (
     <div>
       <Header name="Voltar" className="button-back" />
@@ -26,7 +34,12 @@ export default function TokenForm({namePage}) {
         style={{ display: "flex" }}
       >
         <p style={{ fontWeight: "bold" }}>{namePage}</p>
-        <Button size="sm" variant="contained" className="btnBack">
+        <Button
+          size="sm"
+          variant="contained"
+          className="btnBack"
+          onClick={() => navigate("/")}
+        >
           Voltar
         </Button>
       </Container>
@@ -36,17 +49,29 @@ export default function TokenForm({namePage}) {
         className="form-group"
       >
         <div className="div-label">
-          <label for="token">Token</label>
-          <input type="text" id="token" name="firstname" onChange={({target}) => setInputToken(target.value)}></input>
+          <label htmlFor="token">Token</label>
+          <input
+            type="text"
+            id="token"
+            name="firstname"
+            required
+            onChange={({ target }) => setInputToken(target.value)}
+          ></input>
         </div>
-        <label for="balance">Balance</label>
-        <input type="text" id="balance" name="balance" onChange={ ({target}) => setInputBalance(target.value) }></input>
+        <label htmlFor="balance">Balance</label>
+        <input
+          type="text"
+          id="balance"
+          name="balance"
+          required
+          onChange={({ target }) => setInputBalance(target.value)}
+        ></input>
         <div className="button-save">
           <Button
             size="small"
             variant="contained"
             style={{ backgroundColor: "purple" }}
-            onClick={ addToLocalStorage }
+            onClick={addToLocalStorage}
           >
             Save
           </Button>
